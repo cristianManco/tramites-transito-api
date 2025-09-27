f3602cfbfd1545806c8ba754ddf5812e38319e4d
cristianManco <camilomanco2005@gmail.com>
2025-09-27 10:53:36 -0500

    feat(users): implement UsersService and UsersController with CRUD operations

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -20,31 +20,34 @@ export class UsersController {
 
   @Post('create')
   @ApiResponse({ status: 201, type: UserResponseDto })
-  create(@Body() dto: CreateUserDto) {
-    return this.usersService.create(dto);
+  async create(@Body() dto: CreateUserDto) {
+    return await this.usersService.create(dto);
   }
 
   @Get('all')
   @ApiResponse({ status: 200, type: [UserResponseDto] })
-  findAll() {
-    return this.usersService.findAll();
+  async findAll() {
+    return await this.usersService.findAll();
   }
 
   @Get('details/:id')
   @ApiResponse({ status: 200, type: UserResponseDto })
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.usersService.findOne(id);
   }
 
   @Put('update/:id')
   @ApiResponse({ status: 200, type: UserResponseDto })
-  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateUserDto) {
-    return this.usersService.update(id, dto);
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateUserDto,
+  ) {
+    return await this.usersService.update(id, dto);
   }
 
   @Delete('delete/:id')
   @ApiResponse({ status: 204, description: 'Usuario eliminado' })
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.remove(id);
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return await this.usersService.remove(id);
   }
 }
